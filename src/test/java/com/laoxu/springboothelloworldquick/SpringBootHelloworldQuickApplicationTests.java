697da35634589617a08bf985ef7bde8781006630
suixin <1051513344@qq.com>
2020-02-10 13:16:28 +0800

    全部

---
+++ src/test/java/com/laoxu/springboothelloworldquick/SpringBootHelloworldQuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laoxu.springboothelloworldquick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldQuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
