aaf3e0b99e88c35e9929e1b516737b21c0dfcb3a
suixin <1051513344@qq.com>
2020-02-15 15:30:43 +0800

    更新项目

--- src/main/java/com/laoxu/springboothelloworldquick/controller/HelloWorldController.java
+++ src/main/java/com/laoxu/springboothelloworldquick/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello world";
+        return "你好！jenkins";
     }
 }
